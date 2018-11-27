cf12fd431c0c86d342a98673171e4c89ffe3098a
cartertrafton <cartertrafton@gmail.com>
2018-11-26 23:41:48 -0500

    outlining classes

---
+++ src/library.h
@@ -0,0 +1,78 @@
+/* 
+ * library.h
+ * 
+ * This file contains the class outlines for the various media types of the library.
+ * The base class is media, with the three derived classes beign Book, Movie, and Music.
+ * Each has a set of info special to the class, and a set of functions.
+ * These are the classes that will be stored in the library list.
+ *
+ */
+
+class media 
+{
+	private:
+		/*
+		name
+		quantity
+		due_date
+		*/
+	public:
+		/*
+		constructor
+		deconstructor
+		get
+		set
+		print
+		*/
+};
+
+class book : public media
+{
+	private:
+		/*
+		ISBN
+		author
+		*/
+	public:
+		/*
+		constructor
+		deconstructor
+		get
+		set
+		print
+		*/
+};
+
+class movie : public media
+{
+	private:
+		/*
+		director
+		releaseYear
+		*/
+	public:
+		/*
+		constructor
+		deconstructor
+		get
+		set
+		print
+		*/
+};
+
+class music : public media
+{
+	private:
+		/*
+		artist
+		genre
+		*/
+	public:
+		/*
+		constructor
+		deconstructor
+		get
+		set
+		print
+		*/
+};
---
+++ src/user.h
@@ -0,0 +1,33 @@
+/* 
+ * user.h
+ * 
+ * This file contains the class outline for the user class.
+ * This class will be have information relating to the users data, 
+ * and the media that they are currently taking out from the library,
+ * as well as functions for adding media to and removing media from the 
+ * user's collection.
+ *
+ *
+ */
+
+class user 
+{
+	private:
+		/*
+		ID_number
+		name
+		collection[]
+		*/
+	public:
+		/*
+		constructor
+		deconstructor
+		get
+		set
+		print
+			
+		addMedia to collection
+		removeMedia form collection
+
+		*/
+};
